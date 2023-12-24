6a50aae31d70bb917c1936bbe2f254c0e49ff4b3
azrilaiman <azril@bayam.my>
2023-12-24 19:26:11 +0800

    rebrand logbook layout

--- resources/views/students/logbooks/pdf.blade.php
+++ resources/views/students/logbooks/pdf.blade.php
@@ -18,6 +18,19 @@
         width: 100%;
     }
 
+    .title {
+        margin-left: 50px;
+    }
+
+    .box {
+        border: 1px solid black;
+        padding: 5px;
+        display: inline-block;
+        margin-left: 50px;
+        width: 600px;
+        height: 500px;
+        margin-top: 10px;
+    }
 </style>
 
 <head>
@@ -26,20 +39,66 @@
 
 <body>
     <center>
-        <h1>{{ 'LAPORAN ' }}{{ $categoryLabel }}{{ ' LATIHAN INDUSTRI' }}</h1>
+        <h1>{{ 'LAPORAN ' }}{{ $categoryLabel }}{{ ' LI' }}</h1>
     </center>
     <div>
-        <h3 style="display: inline; margin-right: 0px;">Tarikh:</h3> <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline" style="width: 15%; margin-right: 200px;">{{ $logbook->date }}</span></p>
-        <h3 style="display: inline; margin-right: 0px;">Hari:</h3> <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline" style="width: 15%;">{{ $logbook->hari }}</span></p>
-        <h3 style="display: inline; margin-right: 0px;">Tempat/Lokasi:</h3> <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline" style="width: 64%;">{{ $logbook->location }}</span></p>
-        <h3 style="display: inline; margin-right: 0px;">Tajuk Kerja/Projek:</h3> <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline" style="width: 59%">{{ $logbook->title }}</span></p>
-        <h3 style="display: inline; margin-right: 0px;">Peralatan / Perisian yang digunakan :</h3> <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline" style="width: 39%">{{ $logbook->field1 }}</span></p>
-        <h3 style="display: inline; margin-right: -50px;">Pengujian yang dijalankan</h3>
-        <p style="display: inline;">(sekiranya ada): </p>
-        <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline" style="width: 30%">{{ $logbook->field2 }}</p>
-        <h3 style="display: inline; margin-right: -50px;">Langkah - Langkah Keselamatan</h3>
-        <p style="display: inline;">(sekiranya ada):</p>
-        <h3><span style="text-decoration: underline;">{{ $logbook->field3 }}</span></h3>
+        <h3 style="display: inline; margin-right: 0px; margin-bottom: 10px;">Tarikh:</h3>
+        <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline"
+                style="width: 15%; margin-right: 200px;">{{ $logbook->date }}</span></p>
+        <h3 style="display: inline; margin-right: 0px;">Hari:</h3>
+        <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline"
+                style="width: 15%;">{{ $logbook->hari }}</span></p>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Tempat/Lokasi:</h3>
+            <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;"><span class="underline"
+                    style="width: 64%;">{{ $logbook->location }}</span></p>
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Tajuk Kerja/Projek:</h3>
+        </div>
+        <div class="title">
+            <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;">
+                <span class="underline"
+                    style="width: 89%; margin-top: 10px; margin-bottom: 17px">{{ $logbook->title }}</span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+            </p>
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Peralatan / Perisian / Dokumen yang digunakan :</h3>
+        </div>
+        <div class="title">
+            <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;">
+                <span class="underline"
+                    style="width: 89%; margin-top: 10px; margin-bottom: 17px">{{ $logbook->field1 }}</span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+            </p>
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: -50px;">Pengujian yang dijalankan</h3>
+            <p style="display: inline;">(sekiranya ada): </p>
+        </div>
+        <div class="title">
+            <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;">
+                <span class="underline"
+                    style="width: 89%; margin-top: 10px; margin-bottom: 17px">{{ $logbook->field2 }}</span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+            </p>
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Langkah - Langkah Keselamatan</h3>
+            <p style="display: inline; margin-left: 0px;">(sekiranya ada):</p>
+        </div>
+        <div class="title">
+            <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;">
+                <span class="underline"
+                    style="width: 89%; margin-top: 10px; margin-bottom: 17px">{{ $logbook->field2 }}</span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+            </p>
+        </div>
         <h3>Perincian Kerja/Projek : </h3>
         <p>(Langkah kerja, pengiraan, carta / jadual dan gambar rajah yang bersesuaian perlu disertakan)</p>
         <p><span class="underline" style="margin-top: 17px; margin-bottom: 17px;">{{ $logbook->field4 }}</span></p>
@@ -48,9 +107,32 @@
         <p><span class="underline" style="margin-top: 17px; margin-bottom: 17px;"></span></p>
         <p><span class="underline" style="margin-top: 17px; margin-bottom: 17px;"></span></p>
         <p><span class="underline" style="margin-top: 17px; margin-bottom: 17px;"></span></p>
-        <p><span class="underline" style="margin-top: 17px; margin-bottom: 17px;"></span></p>
+
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Lampiran Perincian Kerja/Projek</h3>
+            <p style="display: inline; margin-left: 0px;">(sekiranya ada):</p>
+        </div>
+        <div class="box">
+            {{-- <p style="display: inline; margin-top: 0px; margin-bottom: 0px; margin-left: 0px;">
+                <span class="underline" style="width: 89%; margin-top: 10px; margin-bottom: 17px">{{ $logbook->title }}</span>
+                <span class="underline" style="width: 89%; margin-top: 17px; margin-bottom: 17px;"></span>
+            </p> --}}
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Disemak Oleh</h3>
+            {{-- <p style="display: inline; margin-left: 0px;">(Tandatangan Pelajar):</p> --}}
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style=" margin-right: 0px;">......................................</h3>
+            <p>Tandatangan, Nama & Cop Penyelia Syarikat</p>
+        </div>
+        <div style="margin-top: 10px;">
+            <h3 style="display: inline; margin-right: 0px;">Tarikh:</h3>
+        </div>
 
     </div>
+
+
 </body>
 
 </html>
