a28e5e6dd78b5ad6461e023ea68dc72d49428a06
azrilaiman <azrilaimankopisan@gmail.com>
2024-04-25 15:55:03 +0800

    Update sidebar.blade.php

--- resources/views/layouts/partials/navigation/sidebar.blade.php
+++ resources/views/layouts/partials/navigation/sidebar.blade.php
@@ -100,7 +100,7 @@
                     'industry' => [
                         'text' => 'Intern',
                         'icon' => '<svg aria-hidden="true" class="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>',
-                        'route' => 'industry.student.index',
+                        'route' => 'industry.view.student',
                     ],
                     'admin' => [
                         'text' => 'Intern',
@@ -110,14 +110,14 @@
                 ];
             @endphp
             @foreach($guardLinks as $guard => $link)
-            {{-- @if(auth()->guard($guard)->check())
+            @if(auth()->guard($guard)->check())
                 <li>
                     <a href="{{ route($link['route']) }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         {!! $link['icon'] !!}
                         <span class="ml-3">{{ $link['text'] }}</span>
                     </a>
                 </li>
-            @endif --}}
+            @endif
         @endforeach
             <li>
                 <a href="#"
