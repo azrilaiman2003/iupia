0aa1f52409431748985f73c51bec9a0757afbf97
Azril Aiman <azril@bayam.my>
2024-04-27 20:53:47 +0800

    Update index.blade.php

--- resources/views/admins/institutions/index.blade.php
+++ resources/views/admins/institutions/index.blade.php
@@ -14,28 +14,31 @@
 
                 <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
                     <div class="sm:col-span-2">
-                        <label for="name"
+                        <label for="logo"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Logo</label>
-                        <div class="flex items-center justify-center w-full">
+                        <div class="">
                             <label for="logo"
-                                class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                class="group p-4 sm:p-7 block cursor-pointer text-center border-2 border-dashed border-gray-200 rounded-lg focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-gray-700">
+                                <div class="">
                                     <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none"
                                         stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12">
                                         </path>
                                     </svg>
-                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
-                                            class="font-semibold">Click to upload</span> or drag and drop</p>
-                                    <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG or JPG (MAX. 800x400px)</p>
+                                    <span class="mt-2 block text-sm text-gray-800 dark:text-gray-200">
+                                        Browse your device
+                                    </span>
+                                    <span class="mt-1 block text-xs text-gray-500">
+                                        Maximum file size is 2 MB
+                                    </span>
                                     <div
-                                    class="group p-4 sm:p-7 mt-5 block cursor-pointer text-center border-2 border-dashed border-gray-200 rounded-lg focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-gray-700">
-                                    <div id="imagePreviewContainer" class="hidden mt-4">
-                                        <img id="imagePreview" class="w-32 h-32 object-cover rounded-lg mx-auto"
-                                            alt="Selected Image">
+                                        class="group p-4 sm:p-7 mt-5 block cursor-pointer text-center border-2 border-dashed border-gray-200 rounded-lg focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-gray-700">
+                                        <div id="imagePreviewContainer" class="hidden mt-4">
+                                            <img id="imagePreview" class="w-32 h-32 object-cover rounded-lg mx-auto"
+                                                alt="Selected Image">
+                                        </div>
                                     </div>
-                                </div>
                                 </div>
                                 <input id="logo" name="logo" type="file" value="asset" class="sr-only">
                             </label>
